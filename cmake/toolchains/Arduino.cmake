0bd69864c94222361f6f426906070c2923e324ea
QueezyTheGreat <queezythegreat@gmail.com>
2011-05-01 22:54:02 +0200

    Eliminated problem with __cxa_pure_virtual linker error with Arduino Core library (-Wl,--gc-sections was needed) Default C/C++ and linker flags are now persistent, stored in the cache by toolchain file

--- cmake/toolchains/Arduino.cmake
+++ cmake/toolchains/Arduino.cmake
@@ -3,12 +3,53 @@ set(CMAKE_SYSTEM_NAME Arduino)
 set(CMAKE_C_COMPILER   avr-gcc)
 set(CMAKE_CXX_COMPILER avr-g++)
 
-set(CMAKE_C_FLAGS   "-g -Os -w -ffunction-sections -fdata-sections")
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}  -fno-exceptions")
+#=============================================================================#
+#                              C Flags                                        #
+#=============================================================================#
+set(ARDUINO_C_FLAGS "-ffunction-sections -fdata-sections")
+set(CMAKE_C_FLAGS                "-g -Os       ${ARDUINO_C_FLAGS}" CACHE STRING "")
+set(CMAKE_C_FLAGS_DEBUG          "-g           ${ARDUINO_C_FLAGS}" CACHE STRING "")
+set(CMAKE_C_FLAGS_MINSIZEREL     "-Os -DNDEBUG ${ARDUINO_C_FLAGS}" CACHE STRING "")
+set(CMAKE_C_FLAGS_RELEASE        "-0s -DNDEBUG -w ${ARDUINO_C_FLAGS}" CACHE STRING "")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "-0s -g       -w ${ARDUINO_C_FLAGS}" CACHE STRING "")
 
-set(CMAKE_EXE_LINKER_FLAGS "-Os -Wl,--gc-sections")
-set(CMAKE_SHARED_LINKER_FLAGS)
-set(CMAKE_MODULE_LINKER_FLAGS)
+#=============================================================================#
+#                             C++ Flags                                       #
+#=============================================================================#
+set(ARDUINO_CXX_FLAGS "${ARDUINO_C_FLAGS} -fno-exceptions")
+set(CMAKE_CXX_FLAGS                "-g -Os       ${ARDUINO_CXX_FLAGS}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG          "-g           ${ARDUINO_CXX_FLAGS}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG ${ARDUINO_CXX_FLAGS}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE        "-0s -DNDEBUG ${ARDUINO_CXX_FLAGS}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-0s -g       ${ARDUINO_CXX_FLAGS}" CACHE STRING "")
+
+#=============================================================================#
+#                       Executable Linker Flags                               #
+#=============================================================================#
+set(ARDUINO_LINKER_FLAGS "-Wl,--gc-sections")
+set(CMAKE_EXE_LINKER_FLAGS                "${ARDUINO_LINKER_FLAGS}" CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG          "${ARDUINO_LINKER_FLAGS}" CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS_MINSIZEREL     "${ARDUINO_LINKER_FLAGS}" CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE        "${ARDUINO_LINKER_FLAGS}" CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "${ARDUINO_LINKER_FLAGS}" CACHE STRING "")
+
+#=============================================================================#
+#                       Shared Lbrary Linker Flags                            #
+#=============================================================================#
+set(CMAKE_SHARED_LINKER_FLAGS                ""                        CACHE STRING "")
+set(CMAKE_SHARED_LINKER_FLAGS_DEBUG          ""                        CACHE STRING "")
+set(CMAKE_SHARED_LINKER_FLAGS_MINSIZEREL     ""                        CACHE STRING "")
+set(CMAKE_SHARED_LINKER_FLAGS_RELEASE        ""                        CACHE STRING "")
+set(CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO ""                        CACHE STRING "")
+
+set(CMAKE_MODULE_LINKER_FLAGS                ""                        CACHE STRING "")
+set(CMAKE_MODULE_LINKER_FLAGS_DEBUG          ""                        CACHE STRING "")
+set(CMAKE_MODULE_LINKER_FLAGS_MINSIZEREL     ""                        CACHE STRING "")
+set(CMAKE_MODULE_LINKER_FLAGS_RELEASE        ""                        CACHE STRING "")
+set(CMAKE_MODULE_LINKER_FLAGS_RELWITHDEBINFO ""                        CACHE STRING "")
+
+
+
 
 set(ARDUINO_PATHS)
 foreach(VERSION RANGE 22 1)
