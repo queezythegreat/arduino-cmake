dbd09c5d85aa8d2881463f00418bf75110ca4748
QueezyTheGreat <queezythegreat@gmail.com>
2012-04-12 23:43:30 +0200

    Eliminated need to set CMAKE_MODULE_PATH

--- cmake/modules/ArduinoToolchain.cmake
+++ cmake/modules/ArduinoToolchain.cmake
@@ -2,3 +2,7 @@ set(CMAKE_SYSTEM_NAME Arduino)
 
 set(CMAKE_C_COMPILER   avr-gcc)
 set(CMAKE_CXX_COMPILER avr-g++)
+
+if(EXISTS  ${CMAKE_CURRENT_LIST_DIR}/Platform/Arduino.cmake)
+    set(CMAKE_MODULE_PATH  ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})  # CMake module search path
+endif()
