efd75399e4fc650a47ab57c0c619a27bc9a52180
James Goppert <james.goppert@gmail.com>
2012-04-20 15:56:11 -0400

    Restored default example.

--- example/Blink/Blink.ino
+++ example/Blink/Blink.ino
@@ -9,28 +9,18 @@
 
 Test1 a;
 
-      void         setup()          {                
+void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
   pinMode(13, OUTPUT);     
   /*
     This is a weird comment
   //*/
-
-    if (0) 
-    {
-    }
 }
 
-                  void 
-
-loop() 
-
-
-           {
+void loop() {
   digitalWrite(13, HIGH);   // set the LED on
   delay(1000);              // wait for a second
   digitalWrite(13, LOW);    // set the LED off
   delay(1000);              // wait for a second
-
-                 }
+}
--- test/Blink2/Blink2.ino
+++ test/Blink2/Blink2.ino
@@ -5,41 +5,32 @@
   This example code is in the public domain.
  */
 
-// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
-/*****************************************************************************
-The init_ardupilot function processes everything we need for an in - air restart
-	We will determine later if we are actually on the ground and process a
-	ground start in that case.
-
-*****************************************************************************/
-
-
 #include "config.h"
 
 Test2 a;
-/****
- new comment
-***/
 
-//******this is a comment*/
-//******this is a comment*/
-void setup() {                
+      void         setup()          {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
   pinMode(13, OUTPUT);     
+  /*
+    This is a weird comment
+  //*/
+
+    if (0) 
+    {
+    }
 }
 
-void loop() {
-/***********/
-//******/
-/*
-more weird comments
-*/////////
+                  void 
+
+loop() 
+
+
+           {
   digitalWrite(13, HIGH);   // set the LED on
   delay(1000);              // wait for a second
   digitalWrite(13, LOW);    // set the LED off
   delay(1000);              // wait for a second
-    /* 
-    this is a weird comment
-    //*/
-}
+
+                 }
