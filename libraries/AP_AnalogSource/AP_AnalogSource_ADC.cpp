de95f17f9047de81fd91d93cbd69fcbeeb55b704
QueezyTheGreat <queezythegreat@gmail.com>
2012-05-02 18:55:29 +0200

    Removed ArduPilot tests

--- libraries/AP_AnalogSource/AP_AnalogSource_ADC.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "AP_AnalogSource_ADC.h"
-
-float AP_AnalogSource_ADC::read(void)
-{
-    float fullscale = _adc->Ch(_ch);
-    float scaled = _prescale * fullscale;
-    return scaled;
-}
--- libraries/AP_Compass/AP_Compass.h
+++
@@ -1,7 +0,0 @@
-// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
-
-/// @file	AP_Compass.h
-/// @brief	Catch-all header that defines all supported compass classes.
-
-#include "AP_Compass_HMC5843.h"
-#include "AP_Compass_HIL.h"
