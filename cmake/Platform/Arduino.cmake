750ce82aecef26a4aa59e902808e045bdfaaf551
TheMiles <miles@coders-of-the-caribbean.org>
2014-05-04 01:17:36 +0200

    Revert "Added proper option names to the generate_arduino_firmware call."
    This reverts commit ece86f67a059ba58d02602ae747a912dcdea7a69.

--- cmake/Platform/Arduino.cmake
+++ cmake/Platform/Arduino.cmake
@@ -563,9 +563,9 @@ function(GENERATE_AVR_FIRMWARE INPUT_NAME)
         PROGRAMMER ${INPUT_PROGRAMMER}
         SERIAL ${INPUT_SERIAL}
         SRCS ${INPUT_SRCS}
-        HDRS ${INPUT_HDRS}
-        LIBS ${INPUT_LIBS}
-        AFLAGS ${INPUT_AFLAGS} )
+        ${INPUT_HDRS}
+        ${INPUT_LIBS}
+        ${INPUT_AFLAGS} )
     
 endfunction()
 
